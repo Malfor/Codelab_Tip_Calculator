2fe9b491161618dfaf8f7fb45faaacd8eb8c6014
Rodolfo Barrios <rodolfo.barrios@avista.co>
2021-02-21 23:24:17 -0500

    Se agrega metodo para ocultar el teclado virtual cuando el usuario ingrese datos en el campo de texto y le de enter

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.tiptime
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
 import kotlin.math.ceil
@@ -16,12 +20,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ ->
+            handleKeyEvent(
+                view,
+                keyCode
+            )
+        }
     }
 
     private fun calculateTip() {
         val cost = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
 
-        if (cost == null || cost == 0.0){
+        if (cost == null || cost == 0.0) {
             displayTip(0.0)
             return
         }
@@ -39,19 +50,28 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTipCost(cost: Double, tipPercentage: Double): Double {
         var tip = cost * tipPercentage
-        if (binding.roundUpSwitch.isChecked){
+        if (binding.roundUpSwitch.isChecked) {
             tip = ceil(tip)
         }
         return tip
     }
 
     private fun calculateTipPercentage(): Double {
-        return when(binding.tipOpcion.checkedRadioButtonId){
+        return when (binding.tipOpcion.checkedRadioButtonId) {
             R.id.option_twenty_percental -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
     }
 
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
 
 }
